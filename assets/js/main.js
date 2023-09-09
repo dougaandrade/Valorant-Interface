8895e460050e0480f5530de7bd8b2455ce4b59e4
D Ө U G <116695856+dougaandrade@users.noreply.github.com>
2023-09-09 01:45:27 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+    $(document).ready(function(){
+        $('.carousel').slick({
+            autoplay: true,
+            autoplaySpeed: 4000,
+            vertical: true,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+  });
